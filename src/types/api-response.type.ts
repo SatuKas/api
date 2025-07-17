cea8b1a5d65d353f80e4f439d989b745843b5c86
arubaya <abi.arubaya@gmail.com>
2025-07-18 00:43:50 +0700

    feat(api): enhance ApiResponse type with response status and extra data fields

--- src/types/api-response.type.ts
+++ src/types/api-response.type.ts
@@ -1,18 +1,34 @@
 import { ExceptionMessage } from 'src/common/enums/message/exception-message.enum';
 import { SuccessMessage } from 'src/common/enums/message/success-message.enum';
 import { ExceptionCode } from 'src/common/enums/response-code/exception-code.enum';
+import { SuccessCode } from 'src/common/enums/response-code/success-code.enum';
+
+export enum ResponseStatus {
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
 
 export interface ResponseData<T> {
   data: T;
   message: ExceptionMessage | SuccessMessage | null;
 }
 
+export interface ExtraDataResponse {
+  limit?: number;
+  page?: number;
+  total?: number;
+  total_pages?: number;
+  next_page?: number;
+  prev_page?: number;
+}
+
 export interface ApiResponse<T> extends ResponseData<T> {
-  status: 'success' | 'error';
+  status: ResponseStatus;
+  code: ExceptionCode | SuccessCode;
   error?: {
-    code: ExceptionCode;
     details?: Array<Record<string, any>>;
   };
   timestamp?: string;
   path?: string;
+  extra_data?: ExtraDataResponse;
 }
