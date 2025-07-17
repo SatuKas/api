7a4c25469a75f4093b716426cb2e3b807c1bfe24
arubaya <abi.arubaya@gmail.com>
2025-07-18 00:44:45 +0700

    feat(enum): update success codes to numeric values for better consistency

--- src/common/enums/response-code/success-code.enum.ts
+++ src/common/enums/response-code/success-code.enum.ts
@@ -1,7 +1,9 @@
 export enum SuccessCode {
-  OK = 'SUCCESS',
-  CREATED = 'CREATED',
-  UPDATED = 'UPDATED',
-  DELETED = 'DELETED',
-  FETCHED = 'FETCHED',
+  OK = '00',
+  CREATED = '01',
+  UPDATED = '02',
+  DELETED = '03',
+  FETCHED = '04',
+  NO_CONTENT = '05',
+  PARTIAL_CONTENT = '06',
 }
